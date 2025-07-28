9ea800c6761e40647f15817530052db4b70f4b9c
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-07-29 00:35:57 +0900

    Add `MachOKit` and `FileIO` to dependencies

--- Package.swift
+++ Package.swift
@@ -4,14 +4,24 @@ import PackageDescription
 
 let package = Package(
     name: "dyld-shared-cache-extractor",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
+        .watchOS(.v6),
+        .tvOS(.v13),
+    ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.0"),
+        .package(url: "https://github.com/p-x9/MachOKit.git", from: "0.37.0"),
+        .package(url: "https://github.com/p-x9/swift-fileio.git", from: "0.11.0")
     ],
     targets: [
         .executableTarget(
             name: "dyld-shared-cache-extractor",
             dependencies: [
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
+                .product(name: "MachOKit", package: "MachOKit"),
+                .product(name: "FileIO", package: "swift-fileio")
             ]
         ),
     ]
