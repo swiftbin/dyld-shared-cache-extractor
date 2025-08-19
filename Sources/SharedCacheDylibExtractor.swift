125a6bb4f5935e95c54a7c4d59c29c49036b1787
p-x9 <50244599+p-x9@users.noreply.github.com>
2025-08-19 22:23:28 +0900

    Implemented `SharedCacheDylibExtractor`

---
+++ Sources/SharedCacheDylibExtractor.swift
@@ -0,0 +1,101 @@
+//
+//  SharedCacheDylibExtractor.swift
+//  dyld-shared-cache-extractor
+//
+//  Created by p-x9 on 2025/08/17
+//  
+//
+
+import Foundation
+import MachOKit
+import FileIO
+
+final class SharedCacheDylibExtractor {
+    typealias FileHandle = MemoryMappedFile
+
+    let outputDirectory: URL
+
+    private let fileManager = FileManager.default
+
+    init(outputDirectory: URL) {
+        self.outputDirectory = outputDirectory
+    }
+
+    func extract(for machO: MachOFile, in cache: FullDyldCache) throws {
+        let path = machO.imagePath
+        let outputURL = outputDirectory.appendingPathComponent(path)
+
+        try fileManager.createDirectory(
+            at: outputURL.deletingLastPathComponent(),
+            withIntermediateDirectories: true
+        )
+
+        fileManager.createFile(
+            atPath: outputURL.path,
+            contents: nil
+        )
+
+        let writeHandle: FileHandle = try .open(
+            url: outputURL,
+            isWritable: true
+        )
+
+        try dylibCreate(
+            for: machO,
+            in: cache,
+            writeHandle: writeHandle
+        )
+    }
+}
+
+extension SharedCacheDylibExtractor {
+    // https://github.com/apple-oss-distributions/dyld/blob/93bd81f9d7fcf004fcebcb66ec78983882b41e71/other-tools/dsc_extractor.cpp#L853
+    private func dylibCreate(
+        for machO: MachOFile,
+        in cache: FullDyldCache,
+        writeHandle: FileHandle
+    ) throws {
+        var additionalSize = 0
+        for segment in machO.segments {
+            guard segment.segmentName != "__LINKEDIT" else { continue }
+            additionalSize += segment.fileSize
+        }
+
+        // Write regular segments into the buffer
+        for segment in machO.segments {
+            guard segment.segmentName != "__LINKEDIT" else { continue }
+
+            guard let fileOffset = cache.fileOffset(
+                of: numericCast(segment.virtualMemoryAddress)
+            ) else { fatalError("Invalid virtual memory address") }
+
+            let data = try cache.fileHandle.readData(
+                offset: numericCast(fileOffset),
+                length: segment.fileSize
+            )
+            try writeHandle.insertData(data, at: writeHandle.size)
+        }
+
+        // optimize linkedit
+        let linkeditOptimizer = LinkeditOptimizer(
+            machO: machO,
+            cache: cache
+        )
+        try linkeditOptimizer.optimizeLoadCommands(
+            writeHandle: writeHandle
+        )
+
+        var newLinkeditData = Data()
+        try linkeditOptimizer.optimizeLinkEdit(
+            newLinkeditData: &newLinkeditData,
+            writeHandle: writeHandle
+        )
+        try writeHandle.insertData(newLinkeditData, at: writeHandle.size)
+
+        // Page align file
+        let pad = (4096 - writeHandle.size % 4096) % 4096
+        if pad != 0 {
+            try writeHandle.insertData(Data(count: pad), at: writeHandle.size)
+        }
+    }
+}
